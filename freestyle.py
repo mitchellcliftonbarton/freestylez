9cfb5ce1153c82c6be35918867a290deefba87f8
Mitchell Barton <mitchell@ringseven.com>
2016-05-31 00:02:09 -0600

    added event scheduler

--- freestyle.py
+++ freestyle.py
@@ -1,2 +1,19 @@
-words = ['hello', 'poop']
-print words[0]
+#importing things
+
+import os
+import sched, time
+
+# Words to use in freestyle
+
+words = ['hello', 'what up babe']
+
+# Timer to say words
+
+s = sched.scheduler(time.time, time.sleep)
+def rap():
+    os.system('say ' + words[1])
+    print words[1]
+    s.enter(1, 1, rap, ())
+
+s.enter(1, 1, rap, ())
+s.run()
