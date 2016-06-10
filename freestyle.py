0a98da0330f75ca25e362a5515a68e498a98d128
Mitchell Barton <mitchell@ringseven.com>
2016-06-09 23:09:49 -0600

    added preservation beat

--- freestyle.py
+++ freestyle.py
@@ -1,7 +1,6 @@
 #importing things
 
 import os, sys, random, pyglet
-# import sched, time
 from random import shuffle
 from random import randint
 from threading import Timer
@@ -13,15 +12,26 @@ pyglet.options['audio'] = ('openal', 'silent')
 # rapperz = ['Alex', 'Agnes', 'Kathy', 'Bruce', 'Fred', 'Junior', 'Princess', 'Vicki', 'Ralph', 'Victoria']
 
 # Words to use in freestyle
-hype = ['about to break it down', 'about to drop some bars', 'lay down the beat', 'drop the beat', 'drop it', 'lets go']
-greetings = ['hello', 'what up', 'sup', 'yep', 'hi', 'whats happening', 'whats up', 'yeah', 'hey', 'uh', 'uh huh', 'yup']
-be = ['is', 'was', 'will be', 'is not', 'was not', 'will not be', 'will never be', 'never was', 'never is', 'always is', 'always will be']
+hype = ['about to break it down', 'about to drop some bars', 'lay down the beat', 'drop the beat', 'drop it', 'lets go', 'spin it', 'lay it down']
+greetings = ['hello,', 'what up', 'what up,', 'sup', 'sup', 'yep', 'hi', 'whats happening', 'whats happening,', 'whats up', 'whats up', 'yeah', 'yeah,', 'hey', 'hey,', 'uh', 'uh huh', 'yup']
+be = ['is', 'was', 'will be', 'is not', 'was not', 'will not be', 'will never be', 'never was', 'never is', 'always is', 'always will be', 'I am', 'I be', 'we be', 'they be']
 r1 = ['mic', 'like', 'spike', 'bike', 'pike', 'turnpike', 'ride', 'slide', 'tide', 'pie', 'my', 'cry', 'tie', 'sty', 'pry', 'dry']
 r2 = ['tree', 'me', 'tee', 'see', 'plea', 'treat', 'meet', 'seat', 'feat', 'beat', 'beats', 'meats', 'pleat', 'treat', 'pee', 'dee', 'flee']
 r3 = ['rock', 'mock', 'tock', 'sock', 'clock', 'knock', 'hard knock', 'socks', 'block', 'blocks', 'pot', 'trot', 'sought']
 r4 = ['bark', 'mark', 'spark', 'stark', 'park', 'lark', 'dark', 'fart', 'part', 'apart', 'dart', 'mart', 'cart', 'tart']
+r5 = ['thug', 'mug', 'tug', 'rug', 'snug', 'pug', 'slug', 'plug', 'sun', 'ton', 'run', 'spun', 'done', 'fun', 'won']
+r6 = ['cat', 'tat', 'tats', 'mat', 'rat', 'splat', 'tap', 'nap', 'rap', 'rapping', 'pat', 'gnat', 'slap', 'trap', 'map', 'sap', 'dat', 'spat', 'bat', 'tap']
+r7 = ['spit', 'knit', 'bit', 'sit', 'pit', 'mit', 'never quit', 'quit', 'tip', 'sip', 'dip', 'fit', 'equip', 'equipped', 'rip', 'ripped']
 
-wordz = [greetings, be, r1, r2, r3]
+# themes
+sports = ['ball', 'sports', 'basket', 'basketball', 'baseball', 'football', 'quarterback', 'coach', 'player', 'game', 'points', 'score', 'play', 'playing', 'soccer']
+fruits = ['banana', 'orange', 'apple', 'pineapple', 'tangerine', 'melon', 'watermelon', 'grape', 'fruit', 'grapefruit', 'lemon', 'lime', 'cherry', 'strawberry', 'raspberry', 'blueberry']
+veggies = ['veggies', 'vegetable', 'carrot', 'tomato', 'lettuce', 'broccoli', 'asparagus', 'pepper', 'mushroom']
+art = ['paint', 'paper', 'canvas', 'pen', 'pencil', 'collage', 'collab', 'piece', 'art', 'photo', 'photograph']
+landscape = ['river', 'mountain', 'plain', 'hill', 'lake', 'stream', 'rock', 'tree', 'bush', 'plant', 'flower', 'valley']
+mystical = ['dragon', 'werewolf', 'wizard', 'godzilla', 'zombie', 'witch', 'dinosaur', 't rex', 'monster', 'alien', 'u f o', 'lifeform']
+
+wordz = [greetings, be, r1, r2, r3, r4, r5, r6, r7, sports, fruits, veggies, art, landscape, mystical]
 
 # beatz
 
@@ -30,18 +40,20 @@ song2 = pyglet.media.load('beats/breathe.mp3')
 song3 = pyglet.media.load('beats/everything.mp3')
 song4 = pyglet.media.load('beats/jazz.mp3')
 song5 = pyglet.media.load('beats/electric.mp3')
+song6 = pyglet.media.load('beats/preservation.mp3')
 player = pyglet.media.Player()
 
 # Play beatz
 
 def beatz():
-    playlist = [song1, song2, song3, song4, song5]
+    playlist = [song1, song2, song3, song4, song5, song6]
     shuffle(playlist)
     player.queue(playlist[0])
     player.queue(playlist[1])
     player.queue(playlist[2])
     player.queue(playlist[3])
     player.queue(playlist[4])
+    player.queue(playlist[5])
     player.play()
     print('playing beatz')
 
@@ -53,35 +65,17 @@ barLen = list(range(1,10))
 
 # lead = random.choice(rapperz)
 
+timez = [.5, .75, 1.0, 1.5, 2.0]
+
 def rap():
     # rando1 = randint(0,4)
-    bar = ' '.join(random.choice(wordz[randint(0,4)]) for _ in range(random.choice(barLen)))
+    bar = ' '.join(random.choice(wordz[randint(0,14)]) for _ in range(random.choice(barLen)))
     # os.system('say -v ' + lead + ' ' + bar)
     os.system('say ' + bar)
-    print(bar + ' rap1')
+    print(bar)
     sys.stdout.flush()
-    t = Timer(1.0, rap)
+    t = Timer(random.choice(timez), rap)
     t.start()
-
-# def rap2():
-#     rando1 = randint(0,2)
-#     bar = ' '.join(random.choice(wordz[rando1]) for _ in range(2))
-#     os.system('say ' + bar)
-#     print(bar + ' rap2')
-#     sys.stdout.flush()
-#     t = Timer(1.5, random.choice(rapping))
-#     t.start()
-
-# def rap3():
-#     rando1 = randint(0,2)
-#     bar = ' '.join(random.choice(wordz[rando1]) for _ in range(3))
-#     os.system('say ' + bar)
-#     print(bar + ' rap2')
-#     sys.stdout.flush()
-#     t = Timer(1.5, random.choice(rapping))
-#     t.start()
-
-# rapping = [rap, rap2, rap3]
 def freestyle():
     beatz()
     t = Timer(1.5, rap)
